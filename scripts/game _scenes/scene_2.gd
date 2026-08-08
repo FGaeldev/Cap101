09e7f7a43f76864b52d3036180f089cd1d6ba286
Francis Gael <francisgaelg@gmail.com>
2026-08-09 05:15:53 +0800

    ...

--- scripts/game _scenes/scene_2.gd
+++ scripts/game _scenes/scene_2.gd
@@ -1,6 +1,8 @@
 extends Node2D
-
+ 
 func _ready() -> void:
 	GameState.current_area = "village"  # TODO: update once scene2 has its own area id
 	AudioManager.play_bgm("village")     # TODO: swap if scene2 needs its own track
 	FadeManager.fade_in(0.6)
+	if not GameState.get_flag("seen_chapter1_scene2"):
+		CutsceneManager.play("chapter1_scene2")
